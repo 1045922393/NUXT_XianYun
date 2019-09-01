7447080463819d333d5c7486b43423da04ee63e8
hWY <1045922393@qq.com>
2019-09-01 15:45:47 +0800

    项目初始化

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>首页</h1>
+    <p>我是段落</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less">
+div {
+  h1 {
+    color: red;
+  }
+  p{
+    color:blue;
+    font-size:22px
+  }
+}
+</style>
---
+++ pages/tal.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div>
+      <span
+        :class="{active:current===index}"
+        v-for="(val,index) in [1,2,3]"
+        :key="index"
+        @click="current=index"
+      >{{val}}</span>
+    </div>
+    <div>
+      <ul v-show="current===0">
+        <li>11</li>
+        <li>12</li>
+        <li>13</li>
+      </ul>
+      <ul v-show="current===1">
+        <li>21</li>
+        <li>22</li>
+        <li>23</li>
+      </ul>
+      <ul v-show="current===2">
+        <li>31</li>
+        <li>32</li>
+        <li>33</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      current: 0
+    };
+  }
+};
+</script>
+
+<style lang="less" scoped>
+span {
+    cursor:pointer;
+    margin:20px
+}
+.active {
+  color: red;
+}
+</style>
